4f307d9e44d78441ce1879e0f40cfb19ba026af3
jigar-greycube <jigar@greycube.in>
2026-07-15 10:24:40 +0530

    feat:fixed hooks file for assests

--- sandeep/hooks.py
+++ sandeep/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/sandeep/css/sandeep.css"
-app_include_js = "/assets/sandeep/js/banner.js"
+app_include_js = "/assets/sandeep/js/sandeep.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/sandeep/css/sandeep.css"
