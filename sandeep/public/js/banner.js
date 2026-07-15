86016deec50dc722a0e64ba00bfafdd051bfdfe3
jigar-greycube <jigar@greycube.in>
2026-07-15 09:15:04 +0530

    minor:fixed banner code

--- sandeep/public/js/banner.js
+++ sandeep/public/js/banner.js
@@ -1,10 +1,10 @@
-// $(document).ready(async function () {
-//     let company = frappe.defaults.get_default("company");
-//     frappe.db.get_value("Company", company, "custom_company_banner_color").then((res) => {
-//         $(".sticky-top").append(`
-//         <div style="text-align: center; font-size: 16px; padding: 8px; background: ${res.message.custom_company_banner_color || "#0f0f0f"}; color: #ffffff;">
-//             Current Session Company is ${company}
-//         </div>
-//     `);
-//     })
-// });
+$(document).ready(async function () {
+    let company = frappe.defaults.get_default("company");
+    frappe.db.get_value("Company", company, "custom_company_banner_color").then((res) => {
+        $(".sticky-top").append(`
+        <div style="text-align: center; font-size: 16px; padding: 8px; background: ${res.message.custom_company_banner_color || "#0f0f0f"}; color: #ffffff;">
+            Current Session Company is ${company}
+        </div>
+    `);
+    })
+});
